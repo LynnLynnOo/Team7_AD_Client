9be9c7be14ee6d2e73d0c15d8cd9b72d82e3f7fb
Teh Li Heng <henrytlh@hotmail.com>
2019-01-21 22:31:24 +0800

    Email function

--- app/src/main/java/com/example/adteam7/team7_ad_client/activities/MainActivity.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.adteam7.team7_ad_client.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,8 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.adteam7.team7_ad_client.R;
+import com.example.adteam7.team7_ad_client.network.SendMailTask;
+
 /**
  * Created by Kay Thi Swe Tun
  **/
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
       //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -36,19 +37,19 @@ public class MainActivity extends AppCompatActivity
             }
         });*/
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -80,14 +81,15 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+            new SendMailTask(MainActivity.this).execute("henrytlh@hotmail.com", "Title", "body");
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -98,7 +100,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
---
+++ app/src/main/java/com/example/adteam7/team7_ad_client/data/GMail.java
@@ -0,0 +1,78 @@
+package com.example.adteam7.team7_ad_client.data;
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GMail {
+
+    final private String emailPort = "587";// gmail's smtp port
+    final private String smtpAuth = "true";
+    final private String starttls = "true";
+    final private String emailHost = "smtp.gmail.com";
+
+    private String fromEmail;
+    private String fromPassword;
+    private String toEmail;
+    private String emailSubject;
+    private String emailBody;
+
+    private Properties emailProperties;
+    private Session mailSession;
+    private MimeMessage emailMessage;
+
+    public GMail() {
+
+    }
+
+    public GMail(String toEmail, String emailSubject, String emailBody) {
+        this.fromEmail = "team7logicdb@gmail.com";
+        this.fromPassword = "SA47Team7LogicDB1";
+        this.toEmail = toEmail;
+        this.emailSubject = emailSubject;
+        this.emailBody = emailBody;
+
+        emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", emailPort);
+        emailProperties.put("mail.smtp.auth", smtpAuth);
+        emailProperties.put("mail.smtp.starttls.enable", starttls);
+        Log.i("GMail", "Mail server properties set.");
+    }
+
+    public MimeMessage createEmailMessage() throws
+            MessagingException, UnsupportedEncodingException {
+
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+        emailMessage = new MimeMessage(mailSession);
+
+        emailMessage.setFrom(new InternetAddress(fromEmail, fromEmail));
+        Log.i("GMail", "toEmail: " + toEmail);
+        emailMessage.addRecipient(Message.RecipientType.TO,
+                new InternetAddress(toEmail));
+
+        emailMessage.setSubject(emailSubject);
+        emailMessage.setContent(emailBody, "text/html");// for a html email
+        // emailMessage.setText(emailBody);// for a text email
+        Log.i("GMail", "Email Message created.");
+        return emailMessage;
+    }
+
+    public void sendEmail() throws MessagingException {
+
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromEmail, fromPassword);
+        Log.i("GMail", "allrecipients: " + emailMessage.getAllRecipients());
+        transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+        transport.close();
+        Log.i("GMail", "Email sent successfully.");
+    }
+
+}
