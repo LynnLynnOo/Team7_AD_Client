eaa430ef8ff7f35b22357349b3a610e99075d3da
KittySwe <kaythiswetun@gmail.com>
2019-01-21 14:11:25 +0800

    dir

---
+++ app/src/main/java/com/example/adteam7/team7_ad_client/adapters/FakeAdapter.java
@@ -0,0 +1,7 @@
+package com.example.adteam7.team7_ad_client.adapters;
+
+/**
+ * Created by dodo
+ **/
+public class FakeAdapter {
+}
---
+++ app/src/main/java/com/example/adteam7/team7_ad_client/utils/ForConstant.java
@@ -0,0 +1,7 @@
+package com.example.adteam7.team7_ad_client.utils;
+
+/**
+ * Created by dodo
+ **/
+public class ForConstant {
+}
