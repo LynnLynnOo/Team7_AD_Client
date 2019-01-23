d2f5cd2ec6dbbfa024581dc1f548e5a352400781
Mr-ZTK <zantunkhine@googlemail.com>
2019-01-23 12:39:07 +0800

    Added Raise PO Related Things

---
+++ app/src/main/java/com/example/adteam7/team7_ad_client/activities/ApproveRejectPO.java
@@ -0,0 +1,15 @@
+package com.example.adteam7.team7_ad_client.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adteam7.team7_ad_client.R;
+
+public class ApproveRejectPO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approve_reject_po);
+    }
+}
