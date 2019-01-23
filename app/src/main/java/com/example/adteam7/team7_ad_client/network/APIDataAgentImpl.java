d38fa59082d28b9e6ac21fdedb2ad7252f2acdc3
Mr-ZTK <zantunkhine@googlemail.com>
2019-01-23 12:42:37 +0800

    Added Raise PO Related Things

--- app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
@@ -22,7 +22,7 @@ import static android.content.ContentValues.TAG;
 public class APIDataAgentImpl implements APIDataAgent {
 
   // static String host = "localhost";
-  static String host = "192.168.1.100";
+  static String host = " 172.17.151.50";
    // http://localhost/Team7API/Token
     static String baseURL;
     static String imageURL;
