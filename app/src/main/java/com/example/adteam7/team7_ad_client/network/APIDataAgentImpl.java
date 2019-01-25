09952ba3da6616f4afdf94631dd485eb9f508ba9
Li Heng Teh <e0320674@u.nus.edu>
2019-01-25 20:22:36 +0800

    renamed file as android cannot compile with resource starting with capital letter

--- app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
@@ -35,7 +35,7 @@ import static android.content.ContentValues.TAG;
 public class APIDataAgentImpl implements APIDataAgent {
 
   // static String host = "localhost";
-  static String host = "192.168.1.71";
+  static String host = "192.168.1.100";
    // http://localhost/Team7API/Token
     static String baseURL;
     static String imageURL;
