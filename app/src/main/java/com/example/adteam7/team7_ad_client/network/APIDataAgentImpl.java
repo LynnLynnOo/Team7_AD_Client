75907e47af5dd299878ffac78f52c13ac59fe14f
宗培 程 <1015440098@qq.com>
2019-01-28 13:47:46 +0800

    raise adjustment

--- app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
@@ -35,7 +35,7 @@ import static android.content.ContentValues.TAG;
 public class APIDataAgentImpl implements APIDataAgent {
 
   // static String host = "localhost";
-   static String host = "172.17.82.83";
+   static String host = "172.17.113.199";
    // http://localhost/Team7API/Token
     static String baseURL;
     static String imageURL;
