f9ad48956308eda4d862731b40274024923439e6
Li Heng Teh <e0320674@u.nus.edu>
2019-02-08 08:22:16 +0800

    change ip

--- app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
@@ -39,7 +39,7 @@ public class APIDataAgentImpl implements APIDataAgent {
 
   // static String host = "localhost";
 
-  static String host = "172.17.82.117";
+  static String host = "172.17.5.251";
 
    // http://localhost/Team7API/Token
     static String baseURL;
