7d278492514e5eb229baf85d95198cdf162c53e5
Mr-ZTK <zantunkhine@googlemail.com>
2019-02-10 15:13:04 +0800

    fixed ackDelivery for multiple items update in warehouse

--- app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
+++ app/src/main/java/com/example/adteam7/team7_ad_client/network/APIDataAgentImpl.java
@@ -35,7 +35,7 @@ import static android.content.ContentValues.TAG;
 
 public class APIDataAgentImpl implements APIDataAgent {
 
-    static String host = "172.17.148.100";
+    static String host = "192.168.1.71";
     //static String host = "172.17.5.251";
     static String baseURL;
     static String imageURL;
